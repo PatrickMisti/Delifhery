f24d3b7da32b48383368c26959c5df991dfc5fb6
PatrickMisti <patrickm99@hotmail.de>
2025-12-04 22:44:37 +0100

    add nav bar

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MATERIAL_DASHBOARD} from '../../material-import';
+import {NavBar} from '../nav-bar/nav-bar';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     ...MATERIAL_DASHBOARD,
+    NavBar,
   ],
   templateUrl: './dashboard.html',
   styles: ``,
--- src/material-import.ts
+++ src/material-import.ts
@@ -1,13 +1,14 @@
 import {MatIcon} from '@angular/material/icon';
 import {MatInput} from '@angular/material/input';
-import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatButton, MatFabButton, MatIconButton} from '@angular/material/button';
 import {MatToolbar} from '@angular/material/toolbar';
 
 export const MATERIAL_BASICS = [
   MatIcon,
   MatIconButton,
   MatButton,
-  MatInput
+  MatInput,
+  MatFabButton
 ];
 
 export const MATERIAL_DASHBOARD = [
