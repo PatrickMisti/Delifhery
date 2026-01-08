75bfbca3af2b69f9814c82c9863b8b6e81194d21
PatrickMisti <patrickm99@hotmail.de>
2026-01-08 22:39:12 +0100

    update guard

--- src/core/services/can-activate-user-guard.service.ts
+++ src/core/services/can-activate-user-guard.service.ts
@@ -1,13 +1,9 @@
 import {inject} from '@angular/core';
 import {LoginService} from './login.service';
-import {
-  CanActivateFn,
-  Router,
-} from '@angular/router';
+import {CanActivateFn} from '@angular/router';
 
 export const authGuard: CanActivateFn = () => {
   const auth = inject(LoginService);
-  const router = inject(Router);
   console.log("Auth Guard: is logged in? ", auth.isLoggedIn());
   if (!auth.isLoggedIn()) {
     auth.login();
